b4fc051fae6a0c9369ec11ebfbcac50f0355dda2
YeWint Aung <ye@YeWints-MBP.attlocal.net>
2024-01-21 03:45:14 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const cityRouter = require('./controllers/cityRouter');
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (_, res) => res.redirect('/api'));
+app.use('/api', cityRouter)
+
+app.listen(3001, () => console.log('listening on port http://localhost:3001'))
+
+module.exports = app;
---
+++ controllers/cityRouter.js
@@ -0,0 +1,30 @@
+const cityRouter = require('express').Router()
+const { getParks } = require('../lib/yelp.js')
+const { getWeather } = require('../lib/weather.js')
+const {  getParksFromDB, getParkFromDB, postParksToDB } = require('../lib/supabase.js')
+
+// pull from supabase
+// if not in supabase, request api, then add to supabase
+cityRouter.get('/:state/:city/parks', async (req, res) => {
+  const state = req.params.state, city = req.params.city
+  console.log(city)
+  let parks
+  try {
+    parks = await getParksFromDB(city, state)
+  } catch {
+    parks = await getParks(city, state)
+    await postParksToDB(parks)
+  }
+
+  res.json(parks)
+});
+
+// server side cache this
+cityRouter.get('/:state/:city/weather', async (req, res) => {
+  const state = req.params.state, city = req.params.city
+
+  const weather = await getWeather(city, state)
+  res.json(weather)
+});
+
+module.exports = cityRouter;
